c0268bec2326eea5d48c752c436abc664a7c8d16
Paul Crescini <pcrescini@gmail.com>
2023-09-19 14:36:36 -0500

    update: added not-found.js to render 404 page

---
+++ src/app/not-found.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const metadata = {
+  title: '404 Not found • Paul C Crescini | Front-end Web Developer',
+};
+
+export default function NotFound() {
+  return (
+    <section>
+      <h1>404 Not Found</h1>
+      <p>This page does not exist. Please check the URL and try again.</p>
+    </section>
+  );
+}
