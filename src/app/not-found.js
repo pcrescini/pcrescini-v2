fe5149d55d5f66e5ebd7ecfdbb235ce55f3f98fb
Paul Crescini <pcrescini@gmail.com>
2023-09-19 19:04:41 -0500

    update: changed text copy and added styles to not-found.js

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Link from 'next/link';
+
+import styles from './page.module.css';
 
 export const metadata = {
   title: '404 Not found • Paul C Crescini | Front-end Web Developer',
@@ -6,9 +9,14 @@ export const metadata = {
 
 export default function NotFound() {
   return (
-    <section>
-      <h1>404 Not Found</h1>
-      <p>This page does not exist. Please check the URL and try again.</p>
+    <section id="notFound">
+      <h1 className={styles.colorCelestialBlue}>404 Not Found</h1>
+      <h2 className={styles.colorFederalBlue}>Oh, this is embarrassing...</h2>
+      <p>
+        I can't seem to find the page you requested. While I work on finding it,
+        please check out my home page to see what is currently available.
+      </p>
+      <Link href={`/`}>Take Me Home &rarr; </Link>
     </section>
   );
 }
