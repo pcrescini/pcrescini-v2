058eb972d7cde087f8b7e19db4cc3a4b16ee462f
Paul Crescini <pcrescini@gmail.com>
2023-10-04 08:57:19 -0500

    fix: udated not-found.js and related css to utilize light/dark mode theme

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -3,15 +3,11 @@ import Link from 'next/link';
 
 import styles from './page.module.css';
 
-export const metadata = {
-  title: '404 Not found • Paul C Crescini | Front-end Web Developer',
-};
-
 export default function NotFound() {
   return (
     <section id="notFound">
-      <h1 className={styles.colorCelestialBlue}>404 Not Found</h1>
-      <h2 className={styles.colorFederalBlue}>Oh, this is embarrassing...</h2>
+      <h1 className={styles.primary}>404 Not Found</h1>
+      <h2 className={styles.secondary}>Oh, this is embarrassing...</h2>
       <p>
         I can&#39;t seem to find the page you requested. While I work on finding
         it, please check out my home page to see what is currently available.
